574db5997ea2f592d879194435fa3136df788379
Moh-Khosravi <mohammad.khosravi@gmx.de>
2022-04-05 09:42:15 +0200

    express validator - User register

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/users.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { validationResult } from "express-validator";
 
 // get User
 
@@ -67,7 +68,10 @@ export async function login(req, res) {
 // Register
 
 export async function register(req, res) {
-    /*     if (
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+        /*     if (
         !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/? ]).{8,}$/.test(
           password
         )
@@ -79,27 +83,40 @@ export async function register(req, res) {
           );
         return;
       } */
-    const { firstName, lastName, userName, city, country, email, password } =
-        req.body;
-    const user = await User.findOne({ email });
-    if (user) {
-        res.status(400).send("User already exists");
-        return;
-    }
-    const newPassword = await bcrypt.hash(password, 10);
-
-    try {
-        await User.create({
+        const {
             firstName,
             lastName,
-            email,
-            password: newPassword,
+            userName,
             city,
             country,
-            userName,
+            email,
+            password,
+        } = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            res.status(400).send("User already exists");
+            return;
+        }
+        const newPassword = await bcrypt.hash(password, 10);
+
+        try {
+            await User.create({
+                firstName,
+                lastName,
+                email,
+                password: newPassword,
+                city,
+                country,
+                userName,
+            });
+            res.status(201).send("User created");
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    } else {
+        res.status(400).json({
+            message: "Validation failed",
+            errors: errors.array(),
         });
-        res.status(201).send("User created");
-    } catch (err) {
-        res.status(400).send(err);
     }
 }
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,10 +1,45 @@
 import { Router } from "express";
 import { getUser, login, register } from "../controllers/users.controller.js";
+import { body } from "express-validator";
 
 const routerUsers = new Router();
 
 routerUsers.route("/login").post(login);
-routerUsers.route("/register").post(register);
+routerUsers.route("/register").post(
+    body("firstName")
+        .isString()
+        .isLength({ min: 3, max: 20 })
+        .withMessage(
+            "First name must be between 3 and 20 characters and contains only letters"
+        ),
+    body("lastName")
+        .isString()
+        .isLength({ min: 3, max: 20 })
+        .withMessage(
+            "Last name must be between 3 and 20 characters and contains only letters"
+        ),
+    body("userName")
+        .isAlphanumeric()
+        .isLength({ min: 3, max: 10 })
+        .withMessage(
+            "User name must be between 3 and 10 characters and contains only letters and numbers"
+        ),
+    body("email").isEmail().withMessage("Email must be a valid email address"),
+    body("city")
+        .isString()
+        .isLength({ min: 3, max: 30 })
+        .withMessage(
+            "City must be between 3 and 30 characters and contains only letters"
+        ),
+    body("country")
+        .isString()
+        .isLength({ min: 3, max: 30 })
+        .withMessage(
+            "Country must be between 3 and 30 characters and contains only letters"
+        ),
+
+    register
+);
 routerUsers.route("/:id").get(getUser);
 
 export default routerUsers;
