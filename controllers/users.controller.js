9273bd2a0992a39c309a789e85555d6bc6d1c671
Moh-Khosravi <mohammad.khosravi@gmx.de>
2022-05-04 11:43:26 +0200

    bug fixed

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -113,7 +113,10 @@ export async function getAllUser(req, res) {
 
 export async function login(req, res) {
     const { userName, password } = req.body;
-    const user = await User.findOne({ userName });
+    const user = await User.findOne({ userName })
+        .populate("seedMoney")
+        .populate("budget")
+        .exec();
     const aggregateFriends = await Friend.aggregate([
         {
             $match: {
@@ -143,6 +146,8 @@ export async function login(req, res) {
                     userName: user.userName,
                     friends: aggregateFriends,
                     avatar: user.avatar,
+                    seedMoney: user.seedMoney,
+                    budget: user.budget,
                     mapStyle: {
                         name: user.mapStyle.name,
                         link: user.mapStyle.link,
@@ -184,12 +189,10 @@ export async function register(req, res) {
             firstName,
             lastName,
             userName,
-            // city,
-            // country,
             email,
             password,
             mapStyle,
-            home
+            home,
         } = req.body;
         const user = await User.findOne({ email });
         if (user) {
@@ -204,8 +207,6 @@ export async function register(req, res) {
                 lastName,
                 email,
                 password: newPassword,
-                // city,
-                // country,
                 userName,
                 mapStyle,
                 home,
